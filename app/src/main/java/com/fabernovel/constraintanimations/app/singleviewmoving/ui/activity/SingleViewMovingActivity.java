07e4c08a327334e4f817337ce2aab4e238fbc244
Kélian Clerc <kelian.clerc@applidium.com>
2017-12-14 15:28:30 +0100

    Setup layouts and cycle

--- app/src/main/java/com/fabernovel/constraintanimations/app/singleviewmoving/ui/activity/SingleViewMovingActivity.java
+++ app/src/main/java/com/fabernovel/constraintanimations/app/singleviewmoving/ui/activity/SingleViewMovingActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.widget.Toolbar;
+import android.transition.TransitionManager;
 import android.view.View;
 
 import com.fabernovel.constraintanimations.R;
@@ -14,10 +17,20 @@ import com.fabernovel.constraintanimations.di.ComponentManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SingleViewMovingActivity extends BaseActivity implements SingleViewMovingViewContract {
 
     @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.constraint) ConstraintLayout constraint;
+
+    private boolean hasActivated;
+    private final ConstraintSet constraintSet1 = new ConstraintSet();
+    private final ConstraintSet constraintSet2 = new ConstraintSet();
+    private final ConstraintSet constraintSet3 = new ConstraintSet();
+    private final ConstraintSet constraintSet4 = new ConstraintSet();
+    private final ConstraintSet constraintSet5 = new ConstraintSet();
+    int state;
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, SingleViewMovingActivity.class);
@@ -32,12 +45,21 @@ public class SingleViewMovingActivity extends BaseActivity implements SingleView
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupView();
+        setupAnimation();
         setupToolbar();
     }
 
     private void setupView() {
         setContentView(R.layout.activity_single_moving);
         ButterKnife.bind(this);
+    }
+
+    private void setupAnimation() {
+        constraintSet1.clone(constraint);
+        constraintSet2.clone(this, R.layout.partial_single_view_0);
+        constraintSet3.clone(this, R.layout.partial_single_view_1);
+        constraintSet4.clone(this, R.layout.partial_single_view_2);
+        constraintSet5.clone(this, R.layout.partial_single_view_3);
     }
 
     private void setupToolbar() {
@@ -57,5 +79,38 @@ public class SingleViewMovingActivity extends BaseActivity implements SingleView
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(android.R.anim.fade_in, R.anim.slide_down);
+    }
+
+    @OnClick(R.id.activate)
+    public void onActivate() {
+        state = (state + 1) % 5;
+        animate();
+    }
+
+    private void animate() {
+        TransitionManager.beginDelayedTransition(constraint);
+
+        ConstraintSet toApply1;
+        switch (state) {
+            case 0:
+                toApply1 = constraintSet1;
+                break;
+            case 1:
+                toApply1 = constraintSet2;
+                break;
+            case 2:
+                toApply1 = constraintSet3;
+                break;
+            case 3:
+                toApply1 = constraintSet4;
+                break;
+            default:
+                toApply1 = constraintSet5;
+                break;
+        }
+        toApply1.applyTo(constraint);
+
+        //TransitionManager.beginDelayedTransition(constraint);
+        //toApply2.applyTo(constraint);
     }
 }
